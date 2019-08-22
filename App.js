8dc7f0997148edd6c0811537433efef5a575ca4a
Rommel Rico <me@rommelrico.com>
2019-08-22 07:15:16 -0700

    :sparkles: App: Using the custom header

--- App.js
+++ App.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
+import Header from "./components/Header";
 
 export default function App() {
   return (
-    <View>
+    <View style={styles.screen}>
+      <Header title="Hello World" />
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    flex: 1
+  }
 });
