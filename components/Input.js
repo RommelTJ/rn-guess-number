03afdac929249a533a8a76f577d2eeffd6c18ff6
Rommel Rico <me@rommelrico.com>
2019-08-27 07:05:34 -0700

    :sparkles: Input: Initial implementation

---
+++ components/Input.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { TextInput, StyleSheet } from 'react-native';
+
+const Input = (props) => {
+  return <TextInput/>;
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default Input;
