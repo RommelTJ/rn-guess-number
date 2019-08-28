b462497e22e355f9840b8ee03bba247abfbe5396
Rommel Rico <me@rommelrico.com>
2019-08-28 07:07:49 -0700

    :sparkles: StartGameScreen: Validating input

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { View, Text, StyleSheet, Button } from 'react-native';
 import Card from "../components/Card";
 import Colors from "../constants/colors";
 import Input from "../components/Input";
 
 const StartGameScreen = (props) => {
+
+  const [enteredValue, setEnteredValue] = useState('');
+
+  const numberInputHandler = (inputText) => {
+    setEnteredValue(inputText.replace(/[^0-9]/g, ""));
+  };
+
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game!</Text>
@@ -17,6 +24,8 @@ const StartGameScreen = (props) => {
           autoCorrect={false}
           keyboardType="number-pad"
           maxLength={2}
+          onChangeText={numberInputHandler}
+          value={enteredValue}
         />
         <View style={styles.buttonContainer}>
           <View style={styles.button}>
