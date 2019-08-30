82121646b0aed8817f8a0f4f21f6a6c4c4208486
Rommel Rico <me@rommelrico.com>
2019-08-30 07:19:11 -0700

    :sparkles: StartGameScreen: Displaying an Alert if the number is invalid

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,5 @@
 import React, {useState} from 'react';
-import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { View, Text, StyleSheet, Button, TouchableWithoutFeedback, Keyboard, Alert } from 'react-native';
 import Card from "../components/Card";
 import Colors from "../constants/colors";
 import Input from "../components/Input";
@@ -21,7 +21,10 @@ const StartGameScreen = (props) => {
 
   const confirmInputHandler = () => {
     const chosenNumber = parseInt(enteredValue);
-    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) return undefined;
+    if (isNaN(chosenNumber) || chosenNumber <= 0 || chosenNumber > 99) {
+      Alert.alert("Invalid number", "Number has to be a number between 1 and 99.", [{text:  "OK", style: "destructive", onPress: resetInputHandler}])
+      return undefined;
+    }
 
     setConfirmed(true);
     setSelectedNumber(parseInt(enteredValue)); // this is fine because React will batch these.
