875d5abaae343c22bd1f5266adaafdd9fe97d8c3
Rommel Rico <me@rommelrico.com>
2019-08-31 09:15:17 -0700

    :sparkles: GameOverScreen: Button for restarting game

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 
 const GameOverScreen = (props) => {
   return (
@@ -7,6 +7,7 @@ const GameOverScreen = (props) => {
       <Text>Game Over!</Text>
       <Text>Number of rounds: {props.roundsNumber}</Text>
       <Text>Number was: {props.userNumber}</Text>
+      <Button title="NEW GAME" onPress={() => {}} />
     </View>
   );
 };
