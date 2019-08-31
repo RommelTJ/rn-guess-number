6fdeb89b751820aecdd7ac04500737d48a6dd9bb
Rommel Rico <me@rommelrico.com>
2019-08-31 09:18:32 -0700

    :sparkles: App: Handling a new game

--- App.js
+++ App.js
@@ -9,9 +9,13 @@ export default function App() {
   const [userNumber, setUserNumber] = useState(undefined);
   const [guessRounds, setGuessRounds] = useState(0);
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0);
+    setUserNumber(undefined);
+  };
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
-    setGuessRounds(0);
   };
 
   const gameOverHandler = (numOfRounds) => {
@@ -21,8 +25,8 @@ export default function App() {
   let content = <StartGameScreen onStartGame={startGameHandler} />;
   if (userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />;
-  } else if (userNumber) {
-    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} />;
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler} />;
   }
 
   return (
