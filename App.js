5759fd1c3e99ef2d0035abb073a455775d28c2c3
Rommel Rico <me@rommelrico.com>
2019-09-01 09:03:34 -0700

    :sparkles: App: Font configuration

--- App.js
+++ App.js
@@ -1,9 +1,17 @@
 import React, {useState} from 'react';
 import { StyleSheet, View } from 'react-native';
+import * as Font from 'expo-font';
 import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
+
+const fetchFonts = () => {
+  return Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState(undefined);
