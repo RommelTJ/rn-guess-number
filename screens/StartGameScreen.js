8d87106053f1668e08f7b6186ea944906598cf12
Rommel Rico <me@rommelrico.com>
2019-09-01 16:24:15 -0700

    :lipstick: StartGameScreen: Adding the font to the text

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -49,7 +49,7 @@ const StartGameScreen = (props) => {
       <View style={styles.screen}>
         <Text style={styles.title}>Start a New Game!</Text>
         <Card style={styles.inputContainer}>
-          <Text>Select a Number</Text>
+          <Text style={styles.text}>Select a Number</Text>
           <Input
             style={styles.input}
             blurOnSubmit
@@ -107,6 +107,9 @@ const styles = StyleSheet.create({
   summaryContainer: {
     margin: 20,
     alignItems: 'center'
+  },
+  text: {
+    fontFamily: 'open-sans'
   }
 });
 
