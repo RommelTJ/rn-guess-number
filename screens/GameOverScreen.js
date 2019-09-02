e2bb3be623e5cb601c06555421404b96218f1211
Rommel Rico <me@rommelrico.com>
2019-09-02 08:45:31 -0700

    :bug: GameOverScreen: TitleText

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { View, Text, StyleSheet, Button, Image } from 'react-native';
+import TitleText from "../components/TitleText";
 import BodyText from "../components/BodyText";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
-      <BodyText>Game Over!</BodyText>
+      <TitleText>Game Over!</TitleText>
       <Image style={styles.image} source={require('../assets/success.png')} />
       <BodyText>Number of rounds: {props.roundsNumber}</BodyText>
       <BodyText>Number was: {props.userNumber}</BodyText>
