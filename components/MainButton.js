eefb8b796631e279509c0e5a289bd3493a219cad
Rommel Rico <me@rommelrico.com>
2019-09-03 04:25:08 -0700

    :sparkles: MainButton: Initial implementation

---
+++ components/MainButton.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const MainButton = (props) => {};
+
+const styles = StyleSheet.create({});
+
+export default MainButton;
