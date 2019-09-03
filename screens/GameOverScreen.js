05764142e91e42dcb42b16c547c7d9e5138031e4
Rommel Rico <me@rommelrico.com>
2019-09-03 04:35:38 -0700

    :sparkles: GameOverScreen: Using the MainButton

--- screens/GameOverScreen.js
+++ screens/GameOverScreen.js
@@ -3,6 +3,7 @@ import { View, Text, StyleSheet, Button, Image } from 'react-native';
 import TitleText from "../components/TitleText";
 import BodyText from "../components/BodyText";
 import Colors from '../constants/colors';
+import MainButton from "../components/MainButton";
 
 const GameOverScreen = (props) => {
   return (
@@ -23,7 +24,7 @@ const GameOverScreen = (props) => {
           <Text style={styles.highlight}> {props.userNumber}</Text>.
         </BodyText>
       </View>
-      <Button title="NEW GAME" onPress={props.onRestart} />
+      <MainButton onPress={props.onRestart} >NEW GAME</MainButton>
     </View>
   );
 };
