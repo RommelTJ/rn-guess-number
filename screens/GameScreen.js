f92dac2a9e95a8b53cbe62c42aa81c5065e72866
Rommel Rico <me@rommelrico.com>
2019-09-06 06:28:49 -0700

    :lipstick: GameScreen: Styling the List Items

--- screens/GameScreen.js
+++ screens/GameScreen.js
@@ -5,6 +5,7 @@ import Card from "../components/Card";
 import DefaultStyles from '../constants/default-styles';
 import MainButton from "../components/MainButton";
 import { Ionicons } from '@expo/vector-icons';
+import BodyText from "../components/BodyText";
 
 const generateRandomBetween = (min, max, exclude) => {
   min = Math.ceil(min);
@@ -17,10 +18,11 @@ const generateRandomBetween = (min, max, exclude) => {
   }
 };
 
-const renderListItem = (value) => {
+const renderListItem = (value, numOfRound) => {
   return (
-    <View key={value}>
-      <Text>{value}</Text>
+    <View key={value} style={styles.listItem}>
+      <BodyText>#{numOfRound}</BodyText>
+      <BodyText>{value}</BodyText>
     </View>
   );
 };
@@ -75,9 +77,11 @@ const GameScreen = (props) => {
           <Ionicons name="md-add" size={24} color="white" />
         </MainButton>
       </Card>
-      <ScrollView>
-        {pastGuesses.map(pastGuess => renderListItem(pastGuess))}
-      </ScrollView>
+      <View style={styles.list}>
+        <ScrollView>
+          {pastGuesses.map(pastGuess => renderListItem(pastGuess))}
+        </ScrollView>
+      </View>
     </View>
   );
 };
@@ -94,6 +98,17 @@ const styles = StyleSheet.create({
     marginTop: 20,
     width: 400,
     maxWidth: '90%'
+  },
+  listItem: {
+    borderColor: '#ccc',
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row'
+  },
+  list: {
+    width: '80%'
   }
 });
 
