1b852226ee1742ab8b6d0fc15cd0ee92ef77a9d6
Rommel Rico <me@rommelrico.com>
2019-09-15 06:49:41 -0700

    :bug: MainButton: Fixing the border radius

--- components/MainButton.js
+++ components/MainButton.js
@@ -6,15 +6,21 @@ const MainButton = (props) => {
   let ButtonComponent = TouchableOpacity;
   if (Platform.OS === "android" && Platform.Version >= 21) ButtonComponent = TouchableNativeFeedback;
   return (
-    <ButtonComponent onPress={props.onPress}>
-      <View style={styles.button}>
-        <Text style={styles.buttonText}>{props.children}</Text>
-      </View>
-    </ButtonComponent>
+    <View style={styles.buttonContainer}>
+      <ButtonComponent onPress={props.onPress}>
+        <View style={styles.button}>
+          <Text style={styles.buttonText}>{props.children}</Text>
+        </View>
+      </ButtonComponent>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
+  buttonContainer: {
+    borderRadius: 25,
+    overflow: "hidden"
+  },
   button: {
     backgroundColor: Colors.primary,
     paddingVertical: 12,
